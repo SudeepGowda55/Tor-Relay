5bd2365c108ae63203fb05c816485b4b7a5548e0
SudeepGowda55 <ssudeepgowda55@gmail.com>
2023-12-27 00:04:35 +0530

    middle node setup code

---
+++ initial-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install unattended-upgrades apt-listchanges -y
+
+sudo su
+
+#Check if these cmds are executed or not. If not, then execute these cmds.
+
+rm -rf /etc/apt/apt.conf.d/50unattended-upgrades
+
+touch /etc/apt/apt.conf.d/50unattended-upgrades
+
+echo 'Unattended-Upgrade::Allowed-Origins {
+    "${distro_id}:${distro_codename}-security";
+    "TorProject:${distro_codename}";
+};
+
+Unattended-Upgrade::Package-Blacklist {
+};
+
+Unattended-Upgrade::Automatic-Reboot "true";' >> /etc/apt/apt.conf.d/50unattended-upgrades
+
+echo 'APT::Periodic::AutocleanInterval "5";' >> /etc/apt/apt.conf.d/20auto-upgrades
+echo 'APT::Periodic::Verbose "1";' >> /etc/apt/apt.conf.d/20auto-upgrades
+
+unattended-upgrade --debug
---
+++ tor.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install apt-transport-https -y
+
+sudo su
+
+#Check if these cmds are executed or not. If not, then execute these cmds.
+
+touch /etc/apt/sources.list.d/tor.list
+
+echo 'deb [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org jammy main' >> /etc/apt/sources.list.d/tor.list
+echo 'deb-src [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org jammy main' >> /etc/apt/sources.list.d/tor.list
+
+wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | tee /usr/share/keyrings/tor-archive-keyring.gpg >/dev/null
+
+apt update
+
+apt install tor deb.torproject.org-keyring
+
+echo 'Nickname  luciferRelay
+ContactInfo lambo@i2pmail.org 
+ORPort      443         
+ExitRelay   0
+SocksPort   0' >> /etc/tor/torrc
+
+echo "" >> /etc/tor/torrc
+
+echo '## MONITORING
+ControlPort 9051
+CookieAuthentication 1' >> /etc/tor/torrc
+
+systemctl enable tor
+systemctl restart tor
+systemctl status tor
+
+apt install nyx -y
+
+nyx
